293022ca451a1f0fe3ecc2cc6805fe4b35e654e8
张杰-长安-前端 <zhangjie2026@changan.com.cn>
2025-07-09 15:41:19 +0800

    feat: test

---
+++ var.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n = 1
+	fmt.Println(n)
+
+	var a, b = 1, 2
+	fmt.Println(a, b)
+
+	var c, d string = "hello", "world"
+	fmt.Println(c, d)
+
+	e := 1
+	fmt.Println(e)
+
+	var x int
+	fmt.Println(x)
+
+	var y string
+	fmt.Println(y)
+}
